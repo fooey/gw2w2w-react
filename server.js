adae315ad0fbfd61416e5cfc507dacea0b614dce
Jason Rushton <github.com@schtuph.com>
2014-11-08 13:18:11 -0700

    install NewRelic

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 "use strict";
 
+if (process.env.NODE_ENV !== 'development') {
+	require('newrelic');
+}
+
 const express = require('express');
 const app = require('./config/express')(express);
 const routes = require('./routes')(express);
 
-app.use(routes)
+app.use(routes);
 
 
 
