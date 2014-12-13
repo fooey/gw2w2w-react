145a2bdc8c4c7596b510813cdac4b228fae1288c
Jason Rushton <github.com@schtuph.com>
2014-12-13 13:57:25 -0700

    use jade for the template for NODE_ENV based toggling of minified scripts

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ module.exports = function(express) {
 	var router = express.Router();
 
 	router.get('/:langSlug(en|de|es|fr)?', function(req, res) {
-		res.sendFile(process.cwd() + '/public/index.html');
+		res.render('index', {});
+		// res.sendFile(process.cwd() + '/public/index.html');
 	});
 
 	router.get('/:langSlug(en|de|es|fr)/:langSlug([a-z-]+)', function(req, res) {
-		res.sendFile(process.cwd() + '/public/index.html');
+		res.render('index', {});
+		// res.sendFile(process.cwd() + '/public/index.html');
 	});
 
 	return router;
