8d8fd073b1d2ba709ec6e8102debb404fbdfd3c3
Jason Rushton <github.com@schtuph.com>
2016-02-01 17:08:13 -0700

    reorganize STATIC data, don't bother with getter functions

--- public/js/src/lib/static/index.js
+++ public/js/src/lib/static/index.js
@@ -1,5 +1,4 @@
 'use strict';
-/*eslint camelcase: 0 */
 
 import _ from 'lodash';
 
@@ -15,74 +14,27 @@ function getWorldLink(langSlug, world) {
 }
 
 
-// function enhanceObjectives(objectives) {
-//     console.log('objectives', objectives);
 
-//     return objectives;
-// }
+export const objectives = STATIC_OBJECTIVES;
+export const langs = STATIC_LANGS;
 
 
-function getMapsMeta() {
-    return [
-        {id: 38, name: 'Eternal Battlegrounds', abbr: 'EB'},
-        {id: 1099, name: 'Red Borderlands', abbr: 'Red'},
-        {id: 1102, name: 'Green Borderlands', abbr: 'Grn'},
-        {id: 1143, name: 'Blue Borderlands', abbr: 'Blu'},
-    ];
-}
+export const worlds = _
+    .chain(STATIC_WORLDS)
+    .keyBy('id')
+    .mapValues((world) => {
+        _.forEach(
+            STATIC_LANGS,
+            (lang) =>
+            world[lang.slug].link = getWorldLink(lang.slug, world)
+        );
+        return world;
+    })
+    .value();
 
 
-function getObjectiveGeo() {
-    return {
-        eb: [[
-            {id: '9', direction: ''},          // stonemist
-        ], [
-            {id: '1', direction: 'N'},         // overlook
-            {id: '17', direction: 'NW'},       // mendons
-            {id: '20', direction: 'NE'},       // veloka
-            {id: '18', direction: 'SW'},       // anz
-            {id: '19', direction: 'SE'},       // ogre
-            {id: '6', direction: 'W'},         // speldan
-            {id: '5', direction: 'E'},         // pang
-        ], [
-            {id: '2', direction: 'SE'},        // valley
-            {id: '15', direction: 'S'},        // langor
-            {id: '22', direction: 'E'},        // bravost
-            {id: '16', direction: 'W'},        // quentin
-            {id: '21', direction: 'N'},        // durios
-            {id: '7', direction: 'SW'},        // dane
-            {id: '8', direction: 'NE'},        // umber
-        ], [
-            {id: '3', direction: 'SW'},        // lowlands
-            {id: '11', direction: 'W'},        // aldons
-            {id: '13', direction: 'S'},        // jerrifer
-            {id: '12', direction: 'N'},        // wildcreek
-            {id: '14', direction: 'E'},        // klovan
-            {id: '10', direction: 'NW'},       // rogues
-            {id: '4', direction: 'SE'},        // golanta
-        ]],
-        bl2: [[
-            {id: '113', direction: 'N'},       // rampart
-            {id: '106', direction: 'W'},       // undercroft
-            {id: '114', direction: 'E'},       // palace
-        ], [
-            {id: '102', direction: 'NW'},      // academy
-            {id: '104', direction: 'NE'},      // necropolis
-            {id: '99', direction: 'N'},        // lab
-            {id: '115', direction: 'NW'},      // hideaway
-            {id: '109', direction: 'NE'},      // refuge
-        ], [
-            {id: '110', direction: 'SW'},      // outpost
-            {id: '105', direction: 'SE'},      // depot
-            {id: '101', direction: 'SW'},      // encamp
-            {id: '100', direction: 'SE'},      // farm
-            {id: '116', direction: 'S'},       // well
-        ]],
-    };
-}
 
-
-export const baseObjectivesMeta = [
+export const objectivesMeta = _.keyBy([
     {map: 'eb', group: 1, id: '9', direction: ''},          // stonemist
     {map: 'eb', group: 2, id: '1', direction: 'N'},         // overlook
     {map: 'eb', group: 2, id: '17', direction: 'NW'},       // mendons
@@ -119,36 +71,64 @@ export const baseObjectivesMeta = [
     {map: 'bl2', group: 3, id: '101', direction: 'SW'},      // encamp
     {map: 'bl2', group: 3, id: '100', direction: 'SE'},      // farm
     {map: 'bl2', group: 3, id: '116', direction: 'S'},       // well
+], 'id');
+
+
+
+export const mapsMeta = [
+    {id: 38, name: 'Eternal Battlegrounds', abbr: 'EB'},
+    {id: 1099, name: 'Red Borderlands', abbr: 'Red'},
+    {id: 1102, name: 'Green Borderlands', abbr: 'Grn'},
+    {id: 1143, name: 'Blue Borderlands', abbr: 'Blu'},
 ];
 
-function getObjectiveMeta() {
-    return _.keyBy(baseObjectivesMeta, 'id');
-}
 
 
 
 
-
-
-export const worlds = _
-    .chain(STATIC_WORLDS)
-    .keyBy('id')
-    .mapValues((world) => {
-        _.forEach(
-            STATIC_LANGS,
-            (lang) =>
-            world[lang.slug].link = getWorldLink(lang.slug, world)
-        );
-        return world;
-    })
-    .value();
-
-
-
-
-export const langs = STATIC_LANGS;
-// export const worlds = worlds;
-export const objectives = STATIC_OBJECTIVES;
-export const objectivesGeo = getObjectiveGeo();
-export const mapsMeta = getMapsMeta();
-export const objectivesMeta = getObjectiveMeta();
+export const objectivesGeo = {
+    eb: [[
+        {id: '9', direction: ''},          // stonemist
+    ], [
+        {id: '1', direction: 'N'},         // overlook
+        {id: '17', direction: 'NW'},       // mendons
+        {id: '20', direction: 'NE'},       // veloka
+        {id: '18', direction: 'SW'},       // anz
+        {id: '19', direction: 'SE'},       // ogre
+        {id: '6', direction: 'W'},         // speldan
+        {id: '5', direction: 'E'},         // pang
+    ], [
+        {id: '2', direction: 'SE'},        // valley
+        {id: '15', direction: 'S'},        // langor
+        {id: '22', direction: 'E'},        // bravost
+        {id: '16', direction: 'W'},        // quentin
+        {id: '21', direction: 'N'},        // durios
+        {id: '7', direction: 'SW'},        // dane
+        {id: '8', direction: 'NE'},        // umber
+    ], [
+        {id: '3', direction: 'SW'},        // lowlands
+        {id: '11', direction: 'W'},        // aldons
+        {id: '13', direction: 'S'},        // jerrifer
+        {id: '12', direction: 'N'},        // wildcreek
+        {id: '14', direction: 'E'},        // klovan
+        {id: '10', direction: 'NW'},       // rogues
+        {id: '4', direction: 'SE'},        // golanta
+    ]],
+    bl2: [[
+        {id: '113', direction: 'N'},       // rampart
+        {id: '106', direction: 'W'},       // undercroft
+        {id: '114', direction: 'E'},       // palace
+    ], [
+        {id: '102', direction: 'NW'},      // academy
+        {id: '104', direction: 'NE'},      // necropolis
+        {id: '99', direction: 'N'},        // lab
+        {id: '115', direction: 'NW'},      // hideaway
+        {id: '109', direction: 'NE'},      // refuge
+    ], [
+        {id: '110', direction: 'SW'},      // outpost
+        {id: '105', direction: 'SE'},      // depot
+        {id: '101', direction: 'SW'},      // encamp
+        {id: '100', direction: 'SE'},      // farm
+        {id: '116', direction: 'S'},       // well
+    ]],
+};
